41108ccbb295255462ab85b819fe769619b0ce21
Leamir <leamir@devmiranda.com.br>
2025-08-11 21:11:18 -0300

    Update get player count to use cache

--- src/utils/getPlayerCount.ts
+++ src/utils/getPlayerCount.ts
@@ -1,11 +1,15 @@
 import { ServerConfig } from '../types';
-import { SAMPQuery } from './sampQuery';
+import { SAMPInfo, SAMPQuery } from './sampQuery';
+import { client as valkey } from './valkey';
+import { TimeUnit } from "@valkey/valkey-glide";
+
 
 interface PlayerCountResult {
   playerCount: number;
   maxPlayers: number;
   name: string;
   isOnline: boolean;
+  isCached: boolean;
 }
 
 const sampQuery = new SAMPQuery();
@@ -13,9 +17,26 @@ const sampQuery = new SAMPQuery();
 export async function getPlayerCount(
   server: ServerConfig,
   guildId: string = 'unknown',
-  isMonitoring: boolean = false
+  isMonitoring: boolean = false,
+  ignoreCache: boolean = false
 ): Promise<PlayerCountResult> {
   try {
+    if (!ignoreCache) {
+      let cachedInfo = await valkey.get(server.id);
+      if (cachedInfo) {
+        const info: SAMPInfo = (JSON.parse(cachedInfo as any)) as SAMPInfo;
+        if (info) {
+          return {
+            playerCount: info.players,
+            maxPlayers: info.maxplayers,
+            name: info.hostname,
+            isOnline: true,
+            isCached: true
+          };
+        }
+      }
+    }
+
     const info = await sampQuery.getServerInfo(server, guildId, isMonitoring);
 
     if (!info) {
@@ -24,15 +45,25 @@ export async function getPlayerCount(
         maxPlayers: 0,
         name: 'Server Offline',
         isOnline: false,
+        isCached: false
       };
     }
+
+    valkey.set(server.id, JSON.stringify(info), {
+      expiry: {
+        type: TimeUnit.Seconds,
+        count: Number(process.env.VALKEY_KEY_EXPIRY_SECONDS) || 60
+      },
+    });
 
     return {
       playerCount: info.players,
       maxPlayers: info.maxplayers,
       name: info.hostname,
       isOnline: true,
+      isCached: false
     };
+
   } catch (error) {
     console.error('Error getting player count:', error);
     return {
@@ -40,6 +71,7 @@ export async function getPlayerCount(
       maxPlayers: 0,
       name: 'Server Offline',
       isOnline: false,
+      isCached: false
     };
   }
 }
