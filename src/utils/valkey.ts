bc096911b2082a3a2ce4106c2777e8dc2cbaa134
Leamir <leamir@devmiranda.com.br>
2025-08-11 21:11:10 -0300

    Valkey connection helper util

---
+++ src/utils/valkey.ts
@@ -0,0 +1,30 @@
+import { GlideClient } from "@valkey/valkey-glide";
+
+// Parse process.env.VALKEY_HOSTS
+/**
+ * #Valkey config
+#Each replica is separated by commas
+#Format is IP:port
+ */
+
+if (process.env.VALKEY_HOSTS == undefined) throw new Error("VALKEY_HOSTS is not defined in environment variables");
+if (process.env.VALKEY_USE_TLS == undefined) throw new Error("VALKEY_USE_TLS is not defined in environment variables");
+
+const addresses = process.env.VALKEY_HOSTS.split(",").map((host) => {
+    const [ip, port] = host.split(":");
+
+    if (ip == null || port == null) 
+        throw new Error(`Invalid VALKEY_HOSTS entry (No IP or port found): ${host}`);
+    
+    return { host: ip, port: parseInt(port) };
+});
+
+export let client: GlideClient;
+
+(async () => {
+    client = await GlideClient.createClient({
+        addresses: addresses,
+        useTLS: process.env.VALKEY_USE_TLS === "true",
+        clientName: "openmonitor",
+    });
+})();
